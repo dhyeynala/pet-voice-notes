a1054dd87f28311a5741f912c2c4b2942cf6c214
DHYEY166 <97232546+DHYEY166@users.noreply.github.com>
2025-06-05 19:31:01 -0700

    pdf

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,15 +1,15 @@
 // public/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+const firebaseConfig = {
+  apiKey: "AIzaSyCVvGsrYlf8rwKVaPlQif8ipW3ghDhBv38",
+  authDomain: "puppypages-29427.firebaseapp.com",
+  databaseURL: "https://puppypages-29427-default-rtdb.firebaseio.com",
+  projectId: "puppypages-29427",
+  storageBucket: "puppypages-29427.appspot.com",
+  messagingSenderId: "168138704553",
+  appId: "1:168138704553:web:69353f37163a17fc1b4bb8",
+  measurementId: "G-5X3SX0RBLK"
 };
 
 export const app = initializeApp(firebaseConfig);
